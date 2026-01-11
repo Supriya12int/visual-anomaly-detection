94188048f9e73759be20be17b981725f21627546
supriya <Supriya12int@example.com>
2026-01-11 17:46:34 +0530

    fix port binding fot render

--- app.py
+++ app.py
@@ -133,4 +133,6 @@ def download_sample():
 
 # -------------------- RUN --------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
